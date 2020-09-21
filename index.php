5e7af7a06ea2c7b2aa4a61f0e592e55c323f9c6a
Benoit BERTRAND <benoit.bertrand@live.fr>
2020-09-21 10:42:18 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Comparaison  Avec Bootstrap</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/main_avecBootstrap.css">
+</head>
+<body>
+<header class="container">
+    <div class="row">
+        <h1 class="col-sm-6 col-sm-push-6 text-right-sm">
+            <strong>Le titre</strong>
+        </h1>
+        <nav class="col-sm-6 col-sm-pull-6 text-center-xs text-left-sm">
+            <div class="navbar-collapse collapse in">
+                <ul class="nav navbar-nav">
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
+<section class="container">
+    <div class="row">
+        <article class="col-sm-6 col-sm-push-6">
+            <p>Le contenu<br>
+                Le contenu<br>
+                Le contenu</p>
+        </article>
+        <aside class="col-sm-6 col-sm-pull-6">Les à coté</aside>
+    </div>
+</section>
+</body>
+<script src="./js/bootstrap.js"></script>
+</html>
